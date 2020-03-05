d16617855fe24732d5ad8d0bb721e97196cec2cc
DustinJSilk <dustinjsilk@gmail.com>
2020-03-05 13:49:23 +0000

    Update Pi 4 touches

--- src/device_handlers/pi_4.ts
+++ src/device_handlers/pi_4.ts
@@ -1,18 +1,18 @@
 import { PiBase, PiHandler } from './pi_base';
 import config from '../config/pi_4.json';
-import { PiMessage } from '../services/midi_server_api';
+import { PiMessage, TouchType } from '../services/midi_server_api';
 import { HueLight } from './hue_light';
 
 /** The range of random MIDI notes to fire when touched. */
 const RANDOM_NOTE_RANGE = 4;
 
 /** How much should the lights hue change when touched. */
-const HUE_CHANGE_DISTANCE = .3;
+const HUE_CHANGE = .2;
 
 export default class Pi extends PiBase implements PiHandler {
   private light = new HueLight('Fruit');
 
-  private lastHue  = this.getRandomHue();
+  private lastHue  = Math.random();
 
   constructor() {
     super(config);
@@ -22,12 +22,14 @@ export default class Pi extends PiBase implements PiHandler {
   }
 
   onMessage(data: PiMessage) {
-    const note = Math.floor(Math.random() * RANDOM_NOTE_RANGE);
-    this.sendNote(36 + note);
+    if (data.type === TouchType.TOUCH) {
+      const note = Math.floor(Math.random() * RANDOM_NOTE_RANGE);
+      this.sendNote(36 + note);
 
-    // Set random hue
-    const hue = this.getNextHueValue();
-    this.light.setHue(hue);
+      // Set random hue
+      const hue = this.getNextHueValue();
+      this.light.setHue(hue);
+    }
   }
 
   /**
@@ -35,19 +37,7 @@ export default class Pi extends PiBase implements PiHandler {
    * value.
    */
   private getNextHueValue(): number {
-    let hue = this.getRandomHue();
-
-    // Simply keep trying until a value is produced with a great enough distance
-    while (Math.abs(hue - this.lastHue) < HUE_CHANGE_DISTANCE) {
-      hue = this.getRandomHue();
-    }
-
-    this.lastHue = hue;
-    return hue;
-  }
-
-  /** Returns a random hue value excluding the last red range: 8%. */
-  private getRandomHue(): number {
-    return Math.random() * 0.92;
+    this.lastHue = (this.lastHue + HUE_CHANGE) % 1;
+    return this.lastHue;
   }
 }

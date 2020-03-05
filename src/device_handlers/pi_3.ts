0e42aba413af1a4361ef277a344f316746f5cff8
DustinJSilk <dustinjsilk@gmail.com>
2020-03-05 15:26:58 +0000

    Update swipe filter values

--- src/device_handlers/pi_3.ts
+++ src/device_handlers/pi_3.ts
@@ -9,10 +9,10 @@ import { tap, debounceTime } from 'rxjs/operators';
 const FILTER_CONTROLLER_ID = 0;
 
 /** The lowest value for the filter. */
-const FILTER_BOTTOM = 50;
+const FILTER_BOTTOM = 20;
 
 /** The highest value for the filter. */
-const FILTER_TOP = 127;
+const FILTER_TOP = 90;
 
 export default class Pi extends PiBase implements PiHandler {
   private light = new HueLight('Swipe');
@@ -20,17 +20,14 @@ export default class Pi extends PiBase implements PiHandler {
   /** Map touch points from top to bottom. */
   private map = [0, 1, 11, 3, 6, 5, 7, 8, 9, 10];
 
-  private updateFilter$ = new Subject<number>();
+  private resetFilter$ = new Subject<number>();
 
   constructor() {
     super(config);
 
-    this.updateFilter$.pipe(
-      // Send filter value
-      tap(value => this.sendFilter(FILTER_CONTROLLER_ID, value)),
-
+    this.resetFilter$.pipe(
       // Wait until values stop emitting for 1.5s
-      debounceTime(1500),
+      debounceTime(800),
 
       // Reset filter to zero
       tap(() => this.sendFilter(FILTER_CONTROLLER_ID, 0)),
@@ -44,11 +41,13 @@ export default class Pi extends PiBase implements PiHandler {
       const value = this.squashValue(touchIndex);
 
       // Send MIDI filter message
-      this.updateFilter$.next(value);
+      this.sendFilter(FILTER_CONTROLLER_ID, value);
 
       // Update hue strip
       const hue = touchIndex / this.map.length;
       this.light.setHue(hue);
+    } else if (data.type === TouchType.RELEASE) {
+      this.resetFilter$.next();
     }
   }
 
